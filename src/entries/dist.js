1953507ca46c74608922f664f51289e971c1c75a
Adam Laird <adam.laird@medicalprotection.org>
2026-04-17 14:37:58 +0100

    fixed missnamed import

--- src/entries/dist.js
+++ src/entries/dist.js
@@ -1,5 +1,5 @@
 import '../styles/h5p-sort-paragraphs-proximity.scss';
-import SortParagraphs from '../scripts/h5p-sort-paragraphs-proximity.js';
+import SortParagraphsProximity from '../scripts/h5p-sort-paragraphs-proximity.js';
 
 // Load library
 H5P.SortParagraphsProximity = SortParagraphsProximity;
